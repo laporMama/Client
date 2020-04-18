bf39ee4e9647e09a0bf68321441452793709deef
reinfajar <tsanimfajar@outlook.com>
2020-04-18 19:15:05 +0700

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/chart/chart.js 	new file:   src/components/chart/chart.vue 	new file:   src/components/parentTable.vue 	modified:   src/router/index.js 	new file:   src/views/parent.vue

---
+++ src/components/chart/chart.vue
@@ -0,0 +1,43 @@
+<template>
+  <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+</template>
+
+<script>
+import PieChart from './PieChart.js'
+export default {
+  name: 'Chart',
+  components: {
+    PieChart
+  },
+  data () {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 20
+      },
+      chartData: {
+        hoverBackgroundColor: 'red',
+        hoverBorderWidth: 10,
+        labels: ['Hadir', 'Absen', 'Sakit', 'Izin'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#2c3e50'],
+            data: [10, 10, 5, 3]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
