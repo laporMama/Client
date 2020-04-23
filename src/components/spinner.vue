c70811ef03da13b2cd19f0dc52385d17155642cb
reinfajar <tsanimfajar@outlook.com>
2020-04-23 16:59:06 +0700

    done

--- src/components/spinner.vue
+++ src/components/spinner.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="vld-parent">
-    <loading :active.sync="isLoading" 
-    :can-cancel="false" 
+    <loading :active.sync="isLoading"
+    :can-cancel="false"
     :is-full-page="fullPage"></loading>
   </div>
 </template>
- 
+
 <script>
-  import Loading from 'vue-loading-overlay'
-  import 'vue-loading-overlay/dist/vue-loading.css'
-  
-  export default {
-    data() {
-      return {
-          isLoading: true,
-          fullPage: true
-      }
-    },
-    components: {
-        Loading
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
+export default {
+  data () {
+    return {
+      isLoading: true,
+      fullPage: true
     }
+  },
+  components: {
+    Loading
   }
-</script> 
+}
+</script>
